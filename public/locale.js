097516d69761ddc342a19c1e99ccb5fcf6ee6e44
tonylua <tonylua@sina.com>
2018-05-15 14:48:18 +0800

    update

--- public/locale.js
+++ public/locale.js
@@ -14,45 +14,7 @@ module.exports = {
     fail: "授权失败",
     extFail: "第三方平台自定义信息失败"
   }
-  /*登录*/
-  , login: {
-  }
-  /*城市选择*/
-  , cities: {
-    geo_notice: '当前定位城市'
-  }
   /*首页*/
   , index: {
-    btn_paihao: '排号',
-    btn_diancan: '点餐',
-    btn_shangcheng: '商城',
-    btn_mendian: '门店'
-  }
-  /*排号*/
-  , paihao: {
-    update: '更新',
-    current: '当前排号',
-    unit_table: '桌',
-    waiting_before: '前面等待',
-    btn_cancel: '放弃排队',
-    btn_update: '更新队列',
-    none_useable: '无可用桌型'
-  }
-  /*点餐*/
-  , diancan: {
-    btn: '点餐',
-    label_loc: '位置',
-    label_table: '桌号'
-  }
-  /*秒杀*/
-  , miaosha: {
-    title: '秒杀',
-    more: '更多'
-  }
-  /*拼团*/
-  , pintuan: {
-    title: '正在拼团',
-    more: '更多',
-    join: '我要参团'
   }
 }
